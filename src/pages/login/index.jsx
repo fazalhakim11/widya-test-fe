a3b59eb0a3ff0da9386d6d6e67d548478ec3873b
Faza Alhakim <fazafal04@gmail.com>
2024-09-06 22:58:44 +0700

    Design Login form

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { loginUser } from "../../stores/slices/authSlice";
 
@@ -18,21 +18,25 @@ const Login = () => {
   };
 
   return (
-    <div className="w-[100%]">
-      <form onSubmit={handleSubmit} className="flex flex-col">
+    <div className="w-[100%] h-[100vh] flex">
+      <form onSubmit={handleSubmit} className="flex flex-col self-center w-[60%] md:w-[30%] h-max p-3 rounded-lg mx-auto bg-[#000]">
+        <h1 className="text-white text-xl font-bold text-center mb-2">Login</h1>
         <input
           type="email"
           placeholder="Email"
           value={email}
+          className="bg-transparent mb-1 text-white focus:outline-none"
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
+          className="bg-transparent mb-5 text-white focus:outline-none"
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Login</button>
+        <button type="submit" className="w-[40%] md:w-[30%] bg-white rounded-lg pb-1 mx-auto mb-3">Login</button>
+        <p className="text-white text-sm">Dont have an account? <Link to="/register" className="font-bold">Register</Link></p>
       </form>
     </div>
   );
